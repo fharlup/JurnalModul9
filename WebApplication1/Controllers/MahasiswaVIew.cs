4a1f56c91d31ae7b12174d82676a5fa76b4a6042
PRAKTIKAN <PRAKTIKAN@DESKTOP-2G1A4E6>
2024-04-29 07:13:05 +0700

    Add project files.

---
+++ WebApplication1/Controllers/MahasiswaVIew.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class MahasiswaController
+{
+
+    static List<string> tes = ["pbo","kpl"];
+    static public List<Mahasiswa> listMhs = new List<Mahasiswa> {
+            new Mahasiswa("arga", "13022223038",2003,tes),
+              new Mahasiswa("fajar", "13022223032",2003,tes),
+               new Mahasiswa("indra", "13022220067",2003,tes),
+
+
+            };
+
+    [HttpGet]
+    public IEnumerable<Mahasiswa> Get()
+    {
+        return listMhs;
+    }
+
+    [HttpGet("{id}")]
+    public Mahasiswa Get(int id)
+    {
+        return listMhs[id];
+    }
+
+    [HttpPost]
+    public void Post([FromBody] Mahasiswa value)
+    {
+        listMhs.Add(value);
+    }
+
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        listMhs.RemoveAt(id);
+    }
+}
---
+++ WebApplication1/Mahasiswa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Mahasiswa
+{
+    public string Nama { get; set; }
+    public string Nim { get; set; }
+    public int year { get; set; }
+    public  List<string> Course { get; set; }
+    public Mahasiswa()
+    {
+    }
+    public Mahasiswa(string Nama, string Nim,int year, List<string> Course)
+    {
+        this.Nama = Nama;
+        this.Nim = Nim;
+        this.year = year;
+        this.Course=Course;
+    }
+}
